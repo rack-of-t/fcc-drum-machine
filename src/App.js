40d7767acd2053ee528c17f7f4d78afc4a955608
rack-of-t <tonyleerackley@gmail.com>
2024-04-17 21:56:37 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import audioClips from './Components/audioClips';
+import { useEffect , useState} from 'react';
 
 function App() {
+const [displayText, setDisplayText] = useState('Text Field');
+
+  useEffect(() => {
+    document.addEventListener('keydown', (event) => {
+      playSound(event.key.toUpperCase());
+    })
+  })
+
+  function playSound(selector,text) {
+    const audio = document.getElementById(selector);
+    audio.play();
+    setDisplayText(text)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <body className='App-body'>
+        <div id="drum-machine">
+          <h1>Drum Machine</h1>
+          <div id="clip-display">{displayText}</div>
+          <div id="clip-bank">
+            {audioClips.map((clip) => (
+              <button 
+                key={clip.url} 
+                className='drum-pad' 
+                id={clip.description} 
+                onClick={() => {
+                  playSound(clip.keyTrigger,clip.description)
+                }
+              }
+              >
+            {clip.keyTrigger}
+            <audio 
+              src={clip.url} 
+              className='clip' 
+              id={clip.keyTrigger}>
+            </audio>
+          </button>
+            ))}            
+          </div>
+          </div>
+      </body>
     </div>
   );
 }
